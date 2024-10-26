aadabd5b7216d73b1e7d06d936d1409da61959e4
againnew97 <160215064+againnew97@users.noreply.github.com>
2024-10-26 21:25:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://whole-natka-idontknowu-893def02.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
